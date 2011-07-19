e7fe19ecb39bab6e558a90d67dc45f9d62349a62
Robert Hromej <robert.hromej@gmail.com>
2011-07-19 09:58:11 +0300

    refactoring

--- config/initializers/mysql_triggers.rb
+++ config/initializers/mysql_triggers.rb
@@ -1,21 +1,23 @@
 begin
-ActiveRecord::Base.connection.execute("DROP TRIGGER if exists comments_compute");
-ActiveRecord::Base.connection.execute <<__SQL
-  CREATE TRIGGER `comments_compute` AFTER INSERT ON `comments`
-  FOR EACH ROW BEGIN
-    UPDATE links SET comments_count=(SELECT COUNT(comments.id) FROM comments WHERE link_id = links.id) WHERE links.id = NEW.link_id;
-  END;
+  ActiveRecord::Base.connection.execute("DROP TRIGGER if exists comments_compute");
+  ActiveRecord::Base.connection.execute <<__SQL
+    CREATE TRIGGER `comments_compute` AFTER INSERT ON `comments`
+    FOR EACH ROW BEGIN
+      UPDATE links SET comments_count=(SELECT COUNT(comments.id) FROM comments WHERE link_id = links.id) WHERE links.id = NEW.link_id;
+    END;
 __SQL
-rescue
+rescue => ex
+  p ex
 end
 
 begin
-ActiveRecord::Base.connection.execute("DROP TRIGGER if exists votes_compute");
-ActiveRecord::Base.connection.execute <<__SQL
-  CREATE TRIGGER `votes_compute` AFTER INSERT ON `votes`
-  FOR EACH ROW BEGIN
-    UPDATE links SET votes_count=(SELECT SUM(votes.kind) FROM votes where link_id=links.id) WHERE links.id = NEW.link_id;
-  END;
+  ActiveRecord::Base.connection.execute("DROP TRIGGER if exists votes_compute");
+  ActiveRecord::Base.connection.execute <<__SQL
+    CREATE TRIGGER `votes_compute` AFTER INSERT ON `votes`
+    FOR EACH ROW BEGIN
+      UPDATE links SET votes_count=(SELECT SUM(votes.kind) FROM votes where link_id=links.id) WHERE links.id = NEW.link_id;
+    END;
 __SQL
-rescue
+rescue => ex
+  p ex
 end
